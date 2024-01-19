5d302ed1284c19057842d42fbfa9019adcc32fbe
Levis <Ngigilevis@gmail.com>
2024-01-19 04:26:59 +0300

    feat:improve script

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
 let weather = {
-    apikey: "336c5ed6d35751cd689cfebce6702ccd"
-    fetchWeather: function() {
-        fetch(
-            "https://api.openweathermap.org/data/2.5/weather?q=" +
-              city +
-              "&units=metric&appid=" +
-              this.apiKey
-          )
-            .then((response) => {
-              if (!response.ok) {
-                alert("No weather found.");
-                throw new Error("No weather found.");
-              }
-              return response.json();
-            })
-            .then((data) => this.displayWeather(data));
-    }
-},
-displayWeather: function (data) {
+  apiKey: "336c5ed6d35751cd689cfebce6702ccd",
+  fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        this.apiKey
+    )
+      .then((response) => {
+        if (!response.ok) {
+          alert("No weather found.");
+          throw new Error("No weather found.");
+        }
+        return response.json();
+      })
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
     const { name } = data;
     const { icon, description } = data.weather[0];
     const { temp, humidity } = data.main;
@@ -34,7 +33,8 @@ displayWeather: function (data) {
     document.querySelector(".weather").classList.remove("loading");
     document.body.style.backgroundImage =
       "url('https://source.unsplash.com/1600x900/?" + name + "')";
-},
-search: function () {
-  this.fetchWeather(document.querySelector(".search-bar").value);
-},
+  },
+  search: function () {
+    this.fetchWeather(document.querySelector(".search-bar").value);
+  },
+};
